d5c463d0667dc40035765dd48aecb20fd57d8298
Ryan R <irishreid55@gmail.com>
2023-06-12 11:53:12 -0700

    udated server.js and other files

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,12 +9,20 @@ const {
     deleteReaction 
 } = require('../../controllers/thought-controller');
 
+// /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
 
 router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
+// /api/thoughts/:thoughtId/reactions
+
 router.route('/:thoughtId/reactions').post(addReaction);
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const db = require('./config/connections');
-const routes = require('./routes');
+const express = require("express");
+const db = require("./config/connections");
+const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
-db.once('open', () => {
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+mongoose.set("debug", true);
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
-
-
-    
